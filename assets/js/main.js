1d79612307b776561c59dbdda57b09b37c4091df
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-04-22 11:05:14 +1000

    uploading

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+/*
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
+
+})(jQuery);
+
+$(document).ready(function() {
+	$(".spotlight .image").css({'height':$(".spotlight .image").first().width() + "px"})
+})
+
+$(window).resize(function() {
+	$(".spotlight .image").css({'height':$(".spotlight .image").first().width() + "px"})
+	// $("h2.contactIcon").css({'height':$("h2.contactIcon").first().width() + "px"})
+})
+
+$('.readMore').click(function(evt) {
+    window.scrollBy({
+        top: $("header#header").innerHeight(),
+        behavior:'smooth'
+    });
+})
---
+++ assets/js/nav.js
@@ -0,0 +1,46 @@
+function toggleNav() {
+    if ($("nav").hasClass("open")) {
+        $("nav").removeClass("open");
+        // $("button#navToggle").css("left", "0");
+    } else {
+        $("nav").addClass("open");
+        // var navWidth = parseInt($("nav").css("width").replace('px', ''));
+        // $("button#navToggle").css({
+        //     transition  :   "left 0.5s",
+        //     left        :   navWidth
+        // });
+    }
+}
+
+$('body').click(function(evt){ 
+    if ($(evt.target).is($("button#navToggle, button#navToggle span"))) {
+        toggleNav();
+    } //else if (!$(evt.target).is($("nav"))) {
+    else if (!$(evt.target).is("nav") && $("nav").hasClass("open")) {
+        //# If any element other than nav menu is clicked
+        toggleNav();
+    }
+});
+
+$('body').on('touchstart', function(evt){ 
+    if ($(evt.target).is($("button#navToggle, button#navToggle span"))) {
+        toggleNav();
+    } //else if (!$(evt.target).is($("nav"))) {
+    else if (!$(evt.target).is("nav") && $("nav").hasClass("open")) {
+        //# If any element other than nav menu is clicked
+        toggleNav();
+    }
+});
+
+$('nav').on('touchstart',function(evt) {
+    evt.stopPropagation();
+})
+
+// $(document).scroll(function() {
+//     // var buttonOffset =  $(document).scrollTop() + 16;
+//     $("button#navToggle").css("top", $(document).scrollTop() + 16);
+//     $("nav").css({
+//         top     :    $(document).scrollTop(),
+//         height  :   '120vh'
+//     }); //120vh because android phones handle vh weirdly with bottom toolbar
+// });
